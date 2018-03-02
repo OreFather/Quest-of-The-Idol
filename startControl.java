471002add13afc6d9b5986712af739e28ce646cf
Colby <ironflame24@yahoo.com>
2018-03-01 18:17:00 -0700

    Basic controle lateyra

---
+++ startControl.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author Colby
+ */
+
+package byui.cit260.Quest_of_the_Ancient_Idol.model;
+
+import modelLayer.Player;
+
+public class startControl {
+    
+    
+    
+    public static void startNewGame(){
+       
+}
+    public static void quitGame() {
+        System.exit(0);
+    }
+
+    public static Game loadSavedGame(String player){
+    
+        
+        return Game;
+    }
+
+    private static class Game {
+
+        public Game() {
+            
+        }
+    }
+}
