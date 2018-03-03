d2c832524a3d492124d18ade29a7a71bcc04dac7
sarahkbarrett <bar15091@byui.edu>
2018-03-02 18:11:12 -0700

    added gamecontrol

---
+++ GameControl.java
@@ -0,0 +1,38 @@
+
+package byui.cit260.Quest_of_the_Ancient_Idol;
+
+public class GameControl {
+    
+    public static String searchRoom(String room) {
+        
+                        
+    }
+    
+    public static String showMap(){
+    
+    }
+   
+       
+     public static void showInventory(String[] args) {
+                inventory();
+            }
+            public static String[] inventory(){
+                String[] A = {"rope", "knife", "flare"};
+                        return A;
+            }
+    
+    public static String help (){
+        
+    }
+    
+    public static void saveGame () {
+        
+    }
+    
+    public static void exitGame () {
+         System.exit(0);
+     }
+    
+    
+    
+}
